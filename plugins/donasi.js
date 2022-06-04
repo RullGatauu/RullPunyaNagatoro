86c8c765df452c6d11683488aa7f71b8cf049694
RullGatauu <106651464+RullGatauu@users.noreply.github.com>
2022-06-04 15:24:03 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,21 +5,8 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let anu = `*─────� DONATE 」 ───*
-
-Hai 👋
-Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├📌Indoosat: 085643354073
-└────
-Berapapun donasi kalian akan sangat berarti 👍
-
-Arigatou!
-
-Contact person Owner:
-wa.me/6285643354073 (Owner)
-
-*donasi via follow ig juga boleh*`
+let anu = `*Donasi Pulsa/Paketan*
+*indosat:* 6285859047172`
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
@@ -29,7 +16,7 @@ wa.me/6285643354073 (Owner)
            hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
-               displayText: '📍instagram',
+               displayText: 'ðŸ“instagram',
                url: instagram
              }
 
