76c7f0f04db9be5ccab8fe347b2bd2693418605d
RullGatauu <106651464+RullGatauu@users.noreply.github.com>
2022-06-04 15:26:52 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,7 +2,7 @@ const { igdl } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
+  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU92D0JKwq/`
   if (!args[0].match(/https:\/\/www.instagram.com\/.*(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
 
   igdl(args[0]).then(async res => {
